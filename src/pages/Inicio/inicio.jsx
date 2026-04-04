fff949d489a79c5c2d1440bb649254d6c0809f5d
Rafael Anjos <anjosrafael59@gmail.com>
2026-04-04 16:48:33 -0300

    estilizando

--- src/pages/Inicio/inicio.jsx
+++ src/pages/Inicio/inicio.jsx
@@ -1,6 +1,24 @@
+import "./inicio.css"
+
 function Inicio() {
     return(
-        <div>Inicio</div>
+        <div className="container-inicio">
+            <div className="navbar-inicio">
+                <ul className="components-navbar">
+                    <li>Feed</li>
+                    <button>+</button>
+                    <li>Explore</li>
+                    <li>Profile</li>
+                </ul>
+            </div>
+            <div className="main-screen">
+                <div className="publication-context">
+                    <div className="publication"></div>
+                </div>
+                <div className="suggestion-context"></div>
+            </div>
+
+        </div>
     )
 }
 
